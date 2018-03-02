6d764b981c8c669c2bc8f8e759790803af2414e6
MrMargouillat <klaja.raphael@gmail.com>
2018-03-02 20:52:51 +0100

    Update

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ module['exports'] = function helloWorld (hook) {
 //   // hook.res is a Node.js httpServer.ServerResponse
 //   // Respond to the request with a simple string
 //   hook.res.json(hook.params);
-    let res=  hook.res 
-    let res = hook.req
     response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
 
 
-    res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-    res.send(JSON.stringify({ "speech": response, "displayText": response 
+    hook.res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+    hook.res.send(JSON.stringify({ "speech": response, "displayText": response 
     //"speech" is the spoken version of the response, "displayText" is the visual version
   }));
 
