1b5b1c97423de20f9047d856f725a3903a155877
MrMargouillat <klaja.raphael@gmail.com>
2018-03-02 20:46:07 +0100

    Initiale

---
+++ index.js
@@ -0,0 +1,19 @@
+// A simple hello world microservice 
+// Service will respond to HTTP requests with a string
+module['exports'] = function helloWorld (hook) {
+//   // hook.req is a Node.js http.IncomingMessage
+//   var host = hook.req.host;
+//   // hook.res is a Node.js httpServer.ServerResponse
+//   // Respond to the request with a simple string
+//   hook.res.json(hook.params);
+    let res=  hook.res 
+    let res = hook.req
+    response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+
+
+    res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+    res.send(JSON.stringify({ "speech": response, "displayText": response 
+    //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
+
+};
